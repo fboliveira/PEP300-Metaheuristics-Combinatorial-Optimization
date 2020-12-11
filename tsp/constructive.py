2345f7089b74ba35a60188cc863303126bb87278
Fernando B Oliveira <fboliveira25@gmail.com>
2020-12-11 15:20:43 -0300

    Scarry functions

--- tsp/constructive.py
+++ tsp/constructive.py
@@ -1,4 +1,7 @@
+import sys
 import random
+import numpy as np
+import copy
 
 def createRandom(dimension):
 
@@ -9,4 +12,43 @@ def createRandom(dimension):
     return solution
 
 def nearestNeighborhood(dimension, distances):
-    pass
+    return createSolution(dimension, distances, 0.0)
+
+def semiGreedyNearNeighborhood(dimension, distances, alpha):
+    return createSolution(dimension, distances, alpha)
+
+def createSolution(dimension, distances, alpha):
+
+    solution = []
+
+    # the first customer is randomly selected
+    solution.append(random.randint(1, dimension))
+
+    for i in range(dimension - 1):
+        # print(solution)
+        # List of distances from solution[i]:
+        dimsol = copy.deepcopy(distances[solution[i] - 1, :])
+        # Remove customers from solution -> set distances to max
+        used = np.asarray(solution)
+        dimsol[ used - 1 ] = sys.maxsize #~Inf
+        # print(dimsol)
+        # Set RCL size
+        # rclSize = min + alpha(max - min)
+        min = 1
+        max = dimension - 1 - i
+        rclSize = int(round(min + alpha * ( max - min )))
+        # print("RCL", rclSize)
+        id = random.randint(0, rclSize - 1)
+        nearest = np.argsort( dimsol )[id] + 1
+        # print(nearest)
+        solution.append(nearest)
+
+    return solution
+
+def test():
+
+    dimension = 5
+    distances = np.random.randint(low=10,high=30, size=(dimension,dimension))
+    print(distances)
+    solution = nearestNeighborhood(dimension, distances)
+    print(solution)
